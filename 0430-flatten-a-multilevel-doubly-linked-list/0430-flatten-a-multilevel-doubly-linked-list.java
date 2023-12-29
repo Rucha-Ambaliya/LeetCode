e15861dbd9062b18ceb8be724fa4f148ad3acc74
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2023-12-29 15:17:08 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (6.46%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -1,3 +1,13 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
 class Solution {
 
     public Node flatten(Node head) {
