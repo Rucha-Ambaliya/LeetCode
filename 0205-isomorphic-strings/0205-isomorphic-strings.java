8981986b2e0680809e7568ef28e46572a9585ecd
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-01-06 22:15:22 +0530

    Time: 23 ms (13.48%), Space: 42.3 MB (27.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,13 +3,16 @@ class Solution {
         Map<Character, Character> mp = new HashMap<>();
         
         for(int i = 0; i < s.length(); i++){
-            char key = s.charAt(i);
-            for(var entry : mp.entrySet()){
-                if(
-                    (key == entry.getKey() && entry.getValue() != t.charAt(i)) ||
-                    (key != entry.getKey() && entry.getValue() == t.charAt(i))
-                ) return false;
-            }
+            if(
+                (
+                    mp.containsKey(s.charAt(i)) && //contains key but not same value
+                    mp.get(s.charAt(i)) != t.charAt(i)
+                ) ||
+                (
+                    !mp.containsKey(s.charAt(i)) && //doesn't contain key but value exist
+                    mp.containsValue(t.charAt(i))
+                )
+            ) return false;
             mp.put(s.charAt(i), t.charAt(i));
         }
         return true;
