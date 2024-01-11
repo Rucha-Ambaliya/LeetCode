c05e2b141f23aaaf172ca86f5354e1791212ad24
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-01-11 22:52:13 +0530

    Time: 1 ms (95.78%), Space: 44.3 MB (35.28%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        List<Integer> helper = new ArrayList<Integer>();
-        boolean[] hasTaken = new boolean[nums.length];
-        getP(nums, ans, helper, hasTaken);
+        getP(nums, ans, 0);
         return ans;
     }
-    public void getP(int[] nums, List<List<Integer>> ans, List<Integer> helper, boolean[] hasTaken){
-        if(helper.size() == nums.length){
-            ans.add(new ArrayList<>(helper));
+    public void getP(int[] nums, List<List<Integer>> ans, int idx){
+        if(idx == nums.length - 1){
+            List<Integer> helper = new ArrayList<>();
+            for(int num : nums){
+                helper.add(num);
+            }
+            ans.add(helper);
             return;
         }
-        for(int i = 0; i < nums.length; i++){
-            if(hasTaken[i]) continue;
-            int num = nums[i];
-            helper.add(num);
-            hasTaken[i] = true;
-            getP(nums, ans, helper, hasTaken);
-            hasTaken[i] = false;
-            helper.remove(helper.size() - 1);
+        for(int i = idx; i < nums.length; i++){
+            swap(i, idx, nums);
+            getP(nums, ans, idx + 1);
+            swap(i, idx, nums);
         }
+    }
+    public void swap(int i, int idx, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[idx];
+        nums[idx] = temp;
     }
 }
