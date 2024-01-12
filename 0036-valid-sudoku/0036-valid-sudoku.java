1322c45c49330ba71f5edf274bb1217b76b07b2c
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-01-12 21:13:27 +0530

    Time: 1 ms (100.00%), Space: 44.9 MB (12.63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,29 +1,37 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int n = board.length;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                char ch = board[i][j];
-                board[i][j] = '.';
-                if(ch != '.' && !isValid(ch, board, i, j)){
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                char currentValue = board[row][col];
+                board[row][col] = '.';
+                if (currentValue != '.' && !isValidValue(currentValue, board, row, col)) {
                     return false;
                 }
-                board[i][j] = ch;
+                board[row][col] = currentValue;
             }
         }
         return true;
     }
-    public boolean isValid(char value, char[][] board, int row, int col){
+
+    public boolean isValidValue(char value, char[][] board, int row, int col) {
         int n = board.length;
-        for(int i = 0; i < n; i++){
-            if(board[row][i] == value) return false;
-            if(board[i][col] == value) return false;
+
+        // Check row and column
+        for (int i = 0; i < n; i++) {
+            if (board[row][i] == value || board[i][col] == value) {
+                return false;
+            }
         }
-        int rGrid = (row/3) * 3;
-        int cGrid = (col/3) * 3;
-        for(int i = rGrid; i < rGrid + 3; i++){
-            for(int j = cGrid; j < cGrid + 3; j++){
-                if(board[i][j] == value) return false;
+
+        // Check subgrid
+        int rowGrid = (row / 3) * 3;
+        int colGrid = (col / 3) * 3;
+        for (int i = rowGrid; i < rowGrid + 3; i++) {
+            for (int j = colGrid; j < colGrid + 3; j++) {
+                if (board[i][j] == value) {
+                    return false;
+                }
             }
         }
         return true;
