13f5a11ae8ecd4c0260973a2cd9c0a8bbbacbf7e
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-02-07 18:52:36 +0530

    Time: 2 ms (90.55%), Space: 46.7 MB (42.32%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,16 +2,8 @@ class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
         for(int i = 1; i < n + 1; i++){
-            ans[i] = cntOnes(i);
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
-    }
-    public int cntOnes(int n){
-        int cnt = 0;
-        while(n != 0){
-            if((n & 1) == 1) cnt++;
-            n = n >> 1;
-        }
-        return cnt;
     }
 }
