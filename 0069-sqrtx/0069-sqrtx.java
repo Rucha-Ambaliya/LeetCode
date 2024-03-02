f74bfc88b25f418c92f530cfd6ac8c99f6d450eb
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-03-02 22:46:25 +0530

    Time: 20 ms (10.44%), Space: 40.6 MB (73.13%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,27 +1,10 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;
+        if (x <= 1) return x;
+        if (x == 2) return 1;
+        for (int i = 1; i < x; i++) {
+            if (x - i * i < 0) return i - 1;
         }
-        
-        long start = 1;
-        long end = x;
-        long result = 0;
-        
-        while (start <= end) {
-            long mid = start + (end - start) / 2;
-            long sqr = mid * mid;
-            
-            if (sqr == x) {
-                return (int) mid;
-            } else if (sqr < x) {
-                start = mid + 1;
-                result = mid;
-            } else {
-                end = mid - 1;
-            }
-        }
-        
-        return (int) result;
+        return -1;
     }
 }
