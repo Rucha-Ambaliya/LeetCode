08de4a393e4ae940fcde46ae2bc6547a92fdae7b
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-03-15 18:15:28 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (61.06%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,8 +1,7 @@
 class Solution {
     public int pivotInteger(int n) {
         int sum = (n * (n + 1)) / 2;
-        double sqrt = Math.sqrt(sum);
-        int roundedSqrt = (int) Math.round(sqrt);
-        return (roundedSqrt * roundedSqrt) == sum ? roundedSqrt : -1;
+        int sqrt = (int)Math.sqrt(sum);
+        return (sqrt * sqrt) == sum ? sqrt : -1;
     }
 }
