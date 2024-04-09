dfb7226c56e90f69a6bc73eaf26dd39f2ce1ff5d
Rucha Ambaliya <148620892+Rucha-Ambaliya@users.noreply.github.com>
2024-04-09 19:10:54 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (55.58%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,15 +1,17 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int sum = 0;
-        int n = tickets.length;
-        int i = 0;
-        while(tickets[k] > 0){
-            if(tickets[i] > 0){
-                sum++;
-                tickets[i]--;
+        int posVal=tickets[k];
+        int sum=0;
+        for(int i = 0; i < tickets.length; i++) {
+            if(tickets[i] >= posVal) {
+                if(i <= k)
+                    sum += posVal;
+                else 
+                    sum += posVal - 1;    
             }
-            i++;
-            if(i == n) i = 0;
+            else {
+                sum += tickets[i];
+            }
         }
         return sum;
     }
